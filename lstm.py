36c3d30005dea5eef26a03d341720e5e35060ec5
Alireza Jalouli <154112092+itsalirezajalouli@users.noreply.github.com>
2024-10-19 21:06:50 +0330

    now we have a dataset class which i understand why we define it that way

--- lstm.py
+++ lstm.py
@@ -6,6 +6,7 @@ import torch.nn as nn
 from copy import deepcopy
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
+from torch.utils.data import Dataset 
 
 #   Device
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -25,7 +26,8 @@ def lookBack(df, nSteps: int, label: str) -> pd.DataFrame:
     df.dropna(inplace = True)
     return df
 
-newData = lookBack(data, 7, 'Close')
+hist = 7
+newData = lookBack(data, hist, 'Close')
 npData = newData.to_numpy()
 
 #   Scale
@@ -33,15 +35,40 @@ scaler = MinMaxScaler(feature_range = (-1, 1))
 npData = scaler.fit_transform(npData)
 
 #   Feats & Targets
-featz = npData[:, 1:]
-featz = deepcopy(np.flip(featz, axis = 1)) #   Because LSTM goes from oldest to latest history summary
-targz = npData[:, 0]                       #   Which is start from -7 to -1
+X = npData[:, 1:]
+X = deepcopy(np.flip(X, axis = 1))     #   Because LSTM goes from oldest to latest summary
+y = npData[:, 0]                       #   Which is start from -7 to -1
 
 #   Split
-splitIdx = int(len(featz) * 0.95)
-xTrain = featz[:splitIdx]
-xTest = featz[splitIdx:]
-yTrain = targz[:splitIdx]
-yTest = targz[splitIdx:]
+splitIdx = int(len(X) * 0.95)
+xTrain = X[:splitIdx]
+xTest = X[splitIdx:]
+yTrain = y[:splitIdx]
+yTest = y[splitIdx:]
 
-#   PyTorch LSTM requires extra dimention
+#   PyTorch LSTM requires extra dimention so here we go...
+xTrain = xTrain.reshape((-1, hist, 1))
+xTest = xTest.reshape((-1, hist, 1))
+yTrain = yTrain.reshape((-1, 1))
+yTest = yTest.reshape((-1, 1))
+
+#   Move 2 torch
+xTrain = torch.tensor(xTrain).float()
+xTest = torch.tensor(xTest).float()
+yTrain = torch.tensor(yTrain).float()
+yTest = torch.tensor(yTest).float()
+
+#   Dataset
+class timeSeriesDataset(Dataset):
+    def __init__(self, X, y) -> None:
+        self.X = X
+        self.y = y
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, i):
+        return self.X[i], self.y[i]
+
+trainSet = timeSeriesDataset(xTrain, yTrain)
+testSet = timeSeriesDataset(xTest, yTest)
