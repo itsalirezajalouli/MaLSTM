fc221f89832945977549a325c8c7cdfe8e51dddc
Alireza Jalouli <154112092+itsalirezajalouli@users.noreply.github.com>
2024-10-19 19:42:22 +0330

    i'm lovin it

--- lstm.py
+++ lstm.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd 
 import torch.nn as nn
 import matplotlib.pyplot as plt
+from copy import deepcopy
 
 #   Device
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -14,5 +15,19 @@ data = data[['Date', 'Close']]
 data['Date'] = pd.to_datetime(data['Date'])
 
 #   Plot
-plt.plot(data['Date'], data['Close'])
-plt.show()
+# plt.plot(data['Date'], data['Close'])
+# plt.show()
+
+#   Lookback function
+def lookBack(df, nSteps: int, label: str) -> pd.DataFrame:
+    df = deepcopy(df)
+    df.set_index('Date', inplace = True)
+    for i in range(1, nSteps + 1):
+        df[f'{label}(t-{i})'] = df[f'{label}'].shift(i)
+    #   Drop Sun & Sat
+    df.dropna(inplace = True)
+    return df
+
+newData = lookBack(data, 7, 'Close')
+print(newData)
+
